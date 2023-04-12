5ec6c85fea99ac11ee047e934ae27ce88ebf986f
KonradChecinski <72439528+KonradChecinski@users.noreply.github.com>
2023-04-12 02:03:07 +0200

    Kolory w theme i UserLayout part2

--- resources/js/Theme/Theme.jsx
+++ resources/js/Theme/Theme.jsx
@@ -9,16 +9,44 @@ import { useMemo } from "react";
 export default function Theme({ children }) {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
+    let lightTheme = {
+        primary: {
+            main: "#1A2035",
+        },
+        // secondary: {},
+        background: {
+            background: "#f3f4f6",
+            default: "#f3f4f6",
+            paper: "#ffffff",
+            card: "#ffffff",
+        },
+    };
+
+    let darkTheme = {
+        primary: {
+            main: "#1A2035",
+        },
+        // secondary: {},
+        background: {
+            background: "#111827",
+            default: "#111827",
+            paper: "#1f2937",
+            card: "#1f2937",
+        },
+    };
+
     const theme = useMemo(
         () =>
             createTheme({
                 palette: {
                     mode: prefersDarkMode ? "dark" : "light",
+                    ...(prefersDarkMode ? { ...darkTheme } : { ...lightTheme }),
                 },
-                // shape: {
-                //     borderRadius: 16,
-                // },
+                shape: {
+                    borderRadius: 16,
+                },
             }),
+
         [prefersDarkMode]
     );
     return (
@@ -28,3 +56,31 @@ export default function Theme({ children }) {
         </ThemeProvider>
     );
 }
+
+//         palette: {
+//         mode,
+//     ...(mode === 'light'
+//             ? {
+//                 // palette values for light mode
+//                 primary: amber,
+//                 divider: amber[200],
+//                 text: {
+//                     primary: grey[900],
+//                     secondary: grey[800],
+//                 },
+//             }
+//             : {
+//                 // palette values for dark mode
+//                 primary: deepOrange,
+//                 divider: deepOrange[700],
+//                 background: {
+//                     default: deepOrange[900],
+//                     paper: deepOrange[900],
+//                 },
+//                 text: {
+//                     primary: '#fff',
+//                     secondary: grey[500],
+//                 },
+//             }),
+//     },
+// });
