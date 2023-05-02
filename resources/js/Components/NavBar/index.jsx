42018308e2410096ef73499583af4aaffe6a1b2e
Konrad Checinski <Konradchecinski2012@gmail.com>
2023-05-02 12:11:01 +0200

    userLayout part9

--- resources/js/Components/NavBar/index.jsx
+++ resources/js/Components/NavBar/index.jsx
@@ -3,20 +3,16 @@ import ApplicationLogo from "@/Components/ApplicationLogo";
 import {
     Badge,
     Card,
-    Divider,
     Grid,
     IconButton,
-    TextField,
     Tooltip,
     InputAdornment,
     FormControl,
     InputLabel,
     OutlinedInput,
-    Menu,
-    MenuItem,
-    Chip,
     ListItemIcon,
     Avatar,
+    Typography,
 } from "@mui/material";
 import { ClickAwayListener } from "@mui/base";
 import { useState } from "react";
@@ -29,6 +25,7 @@ import {
     Logout,
 } from "@mui/icons-material";
 import UserAvatar from "@/Components/UserAvatar";
+import UserAvatarMenu from "@/Components/UserAvatar/Menu";
 
 export default function Navbar({ auth }) {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -41,196 +38,129 @@ export default function Navbar({ auth }) {
     };
 
     return (
-        <Card
-            className="p-2"
-            sx={{
-                display: "flex",
-            }}
-        >
-            <Grid
-                container
-                spacing={1}
-                justifyContent="flex-start"
-                alignItems="center"
+        <>
+            <Card
+                className="p-2"
+                sx={{
+                    display: "flex",
+                }}
             >
-                <Grid item xs={2}>
-                    <FormControl
-                        sx={{ ml: 1, width: "100%" }}
-                        variant="outlined"
-                    >
-                        <InputLabel htmlFor="outlined-adornment-password">
-                            Produkty
-                        </InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            type="text"
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                    // aria-label="toggle password visibility"
-                                    // onClick={handleClickShowPassword}
-                                    // onMouseDown={handleMouseDownPassword}
-                                    >
-                                        <Search />
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            label="Password"
-                        />
-                    </FormControl>
-                </Grid>
-                <Grid item xs={2}>
-                    <FormControl
-                        sx={{ ml: 1, width: "100%" }}
-                        variant="outlined"
-                    >
-                        <InputLabel htmlFor="outlined-adornment-password">
-                            Produkty
-                        </InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            type="text"
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                    // aria-label="toggle password visibility"
-                                    // onClick={handleClickShowPassword}
-                                    // onMouseDown={handleMouseDownPassword}
-                                    >
-                                        <Search />
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            label="Password"
-                        />
-                    </FormControl>
-                </Grid>
                 <Grid
-                    item
-                    xs={8}
-                    justifyContent="flex-end"
-                    alignItems="flex-end"
+                    container
+                    spacing={1}
+                    justifyContent="flex-start"
+                    alignItems="center"
                 >
+                    <Grid item xs={2}>
+                        <FormControl
+                            sx={{ ml: 1, width: "100%" }}
+                            variant="outlined"
+                        >
+                            <InputLabel htmlFor="outlined-adornment-password">
+                                Produkty
+                            </InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type="text"
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        // aria-label="toggle password visibility"
+                                        // onClick={handleClickShowPassword}
+                                        // onMouseDown={handleMouseDownPassword}
+                                        >
+                                            <Search />
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <FormControl
+                            sx={{ ml: 1, width: "100%" }}
+                            variant="outlined"
+                        >
+                            <InputLabel htmlFor="outlined-adornment-password">
+                                Produkty
+                            </InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type="text"
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        // aria-label="toggle password visibility"
+                                        // onClick={handleClickShowPassword}
+                                        // onMouseDown={handleMouseDownPassword}
+                                        >
+                                            <Search />
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                        </FormControl>
+                    </Grid>
                     <Grid
-                        container
-                        spacing={1}
+                        item
+                        xs={8}
                         justifyContent="flex-end"
                         alignItems="flex-end"
                     >
-                        <Grid item xs={1}>
-                            <Tooltip title="Delete">
-                                <IconButton>
-                                    <Badge badgeContent={4} color="primary">
-                                        <Delete />
-                                    </Badge>
-                                </IconButton>
-                            </Tooltip>
-                        </Grid>
-
                         <Grid
-                            item
-                            xs={1}
-                            sx={{
-                                display: "flex",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                position: "relative",
-                            }}
+                            container
+                            spacing={1}
+                            justifyContent="flex-end"
+                            alignItems="flex-end"
                         >
-                            <Tooltip title="Account settings">
-                                <IconButton
-                                    onClick={handleClick}
-                                    sx={{ ml: 2 }}
-                                    aria-controls={
-                                        open ? "account-menu" : undefined
-                                    }
-                                    aria-haspopup="true"
-                                    aria-expanded={open ? "true" : undefined}
-                                >
-                                    <UserAvatar auth={auth} />
-                                </IconButton>
-                            </Tooltip>
+                            <Grid item xs={1}>
+                                <Tooltip title="Delete">
+                                    <IconButton onClick={handleClick}>
+                                        <Badge badgeContent={4} color="primary">
+                                            <Delete />
+                                        </Badge>
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
 
-                            {/*<ClickAwayListener*/}
-                            {/*    mouseEvent="onMouseDown"*/}
-                            {/*    touchEvent="onTouchStart"*/}
-                            {/*    onClickAway={handleClose}*/}
-                            {/*>*/}
-                            <Menu
-                                anchorEl={anchorEl}
-                                id="account-menu"
-                                open={open}
-                                onClose={handleClose}
-                                onClick={handleClose}
-                                hideBackdrop={true}
-                                disableScrollLock
-                                PaperProps={{
-                                    elevation: 0,
-                                    sx: {
-                                        overflow: "visible",
-                                        filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                                        mt: 1.5,
-                                        "& .MuiAvatar-root": {
-                                            width: 32,
-                                            height: 32,
-                                            ml: -0.5,
-                                            mr: 1,
-                                        },
-                                        "&:before": {
-                                            content: '""',
-                                            display: "block",
-                                            position: "absolute",
-                                            top: 0,
-                                            right: 14,
-                                            width: 10,
-                                            height: 10,
-                                            bgcolor: "background.paper",
-                                            transform:
-                                                "translateY(-50%) rotate(45deg)",
-                                            zIndex: 0,
-                                        },
-                                    },
-                                }}
-                                transformOrigin={{
-                                    horizontal: "right",
-                                    vertical: "top",
-                                }}
-                                anchorOrigin={{
-                                    horizontal: "right",
-                                    vertical: "bottom",
+                            <Grid
+                                item
+                                xs={1}
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    position: "relative",
                                 }}
                             >
-                                <Link href={route("profile.edit")}>
-                                    <MenuItem onClick={handleClose}>
-                                        <Avatar /> Profile
-                                    </MenuItem>
-                                </Link>
-
-                                <Divider />
-                                <MenuItem onClick={handleClose}>
-                                    <ListItemIcon>
-                                        <Settings fontSize="small" />
-                                    </ListItemIcon>
-                                    Settings
-                                </MenuItem>
-                                <MenuItem onClick={handleClose}>
-                                    <Link
-                                        href={route("logout")}
-                                        method="post"
-                                        as="button"
+                                <Tooltip title="Account settings">
+                                    <IconButton
+                                        onClick={handleClick}
+                                        sx={{ ml: 2 }}
+                                        aria-controls={
+                                            open ? "account-menu" : undefined
+                                        }
+                                        aria-haspopup="true"
+                                        aria-expanded={
+                                            open ? "true" : undefined
+                                        }
                                     >
-                                        <ListItemIcon>
-                                            <Logout fontSize="small" />
-                                        </ListItemIcon>
-                                        Logout
-                                    </Link>
-                                </MenuItem>
-                            </Menu>
-                            {/*</ClickAwayListener>*/}
+                                        <UserAvatar auth={auth} />
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-        </Card>
+            </Card>
+
+            <UserAvatarMenu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            />
+        </>
     );
 }
