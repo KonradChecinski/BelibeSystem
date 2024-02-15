14bee0e974c43a85202d272cd0f911e116fa57bb
Gromzi <mamgier@gmail.com>
2024-02-15 17:24:09 +0100

    Poprawki w komponencie notatek do klienta

--- resources/js/Components/Pages/Client/NotesClientComponent/index.jsx
+++ resources/js/Components/Pages/Client/NotesClientComponent/index.jsx
@@ -10,46 +10,48 @@ import {Cancel, Save} from "@mui/icons-material";
 import {useNotesClientForm} from "@/Components/Pages/Client/NotesClientComponent/form/useNotesClientForm";
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import TextEditorB2B from "@/Components/TextEditor/B2B";
+import ClientOrderHistoryTable from "@/Components/Table/ClientOrderHistoryTable";
+import ClientNotesTable from "@/Components/Table/ClientNotesTable";
 
 
 export default function NotesClientComponent(props) {
-    const [edited, setEdited] = useState(false);
+    // const [edited, setEdited] = useState(false);
+    //
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     errors: fieldErrors,
+    //     setValue,
+    //     clearErrors,
+    // } = useNotesClientForm()
 
-    const {
-        register,
-        handleSubmit,
-        errors: fieldErrors,
-        setValue,
-        clearErrors,
-    } = useNotesClientForm()
+    // const {data, setData, processing, post} = useForm({
+    //     'notes': '',
+    // })
 
-    const {data, setData, processing, post} = useForm({
-        'notes': '',
-    })
-
-    const initializeFieldValues = () => {
-        setValue('notes', data.notes)
-    }
-
-    useEffect(() => {
-        // inicjacja wartości pól
-        initializeFieldValues()
-    }, [setValue]);
+    // const initializeFieldValues = () => {
+    //     setValue('notes', data.notes)
+    // }
+    //
+    // useEffect(() => {
+    //     // inicjacja wartości pól
+    //     initializeFieldValues()
+    // }, [setValue]);
 
     const onSubmit = (formData) => {
         saveBasic()
     }
 
-    const resetForm = () => {
-        setData({
-            'notes': '',
-        });
-
-        initializeFieldValues()
-        setEdited(false);
-
-        clearErrors('notes')
-    };
+    // const resetForm = () => {
+    //     setData({
+    //         'notes': '',
+    //     });
+    //
+    //     initializeFieldValues()
+    //     setEdited(false);
+    //
+    //     clearErrors('notes')
+    // };
     const saveBasic = () => {
         // post(route("system.products.model.update.basic", {productModel: data.id}), {
         //     onSuccess: params => {
@@ -65,50 +67,55 @@ export default function NotesClientComponent(props) {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-            <Box sx={{display: "flex", flexDirection: "column"}}>
 
-                <Typography
-                    sx={{mb: 3, display: "flex", gap: 1, alignItems: "center"}}>
-                    <NoteAddIcon fontSize={"large"}/>
-                    Notatki do klienta
-                </Typography>
+        <Box sx={{display: "flex", flexDirection: "column"}}>
 
-                <TextEditorB2B
-                    value={data.notes}
-                    setValue={(value) => {
-                        setData("notes", value)
-                    }}
-                    setEdited={setEdited}
-                    readOnly={!props.editing}
-                />
+            <Typography
+                sx={{mb: 3, display: "flex", gap: 1, alignItems: "center"}}>
+                <NoteAddIcon fontSize={"large"}/>
+                Notatki do klienta
+            </Typography>
 
-
-                <Fade in={edited}>
-                    <Button type="submit" variant="outlined" startIcon={<Save/>}
-                            disabled={processing}
-                            sx={{
-                                position: "absolute",
-                                top: 7,
-                                right: 230,
-                            }}>
-                        Zapisz
-                    </Button>
-                </Fade>
-                <Fade in={edited}>
-                    <Button variant="outlined" startIcon={<Cancel/>}
-                            disabled={processing}
-                            sx={{
-                                position: "absolute",
-                                top: 7,
-                                right: 335,
-                            }}
-                            onClick={resetForm}
-                    >
-                        Cofnij zmiany
-                    </Button>
-                </Fade>
+            <Box sx={{pr: 0}}>
+                <ClientNotesTable notes={props.client.notes} readOnly={!props.editing}/>
             </Box>
-        </form>
+
+
+            {/*<TextEditorB2B*/}
+            {/*    value={data.notes}*/}
+            {/*    setValue={(value) => {*/}
+            {/*        setData("notes", value)*/}
+            {/*    }}*/}
+            {/*    setEdited={setEdited}*/}
+            {/*    readOnly={!props.editing}*/}
+            {/*/>*/}
+
+
+            {/*<Fade in={edited}>*/}
+            {/*    <Button type="submit" variant="outlined" startIcon={<Save/>}*/}
+            {/*            disabled={processing}*/}
+            {/*            sx={{*/}
+            {/*                position: "absolute",*/}
+            {/*                top: 7,*/}
+            {/*                right: 230,*/}
+            {/*            }}>*/}
+            {/*        Zapisz*/}
+            {/*    </Button>*/}
+            {/*</Fade>*/}
+            {/*<Fade in={edited}>*/}
+            {/*    <Button variant="outlined" startIcon={<Cancel/>}*/}
+            {/*            disabled={processing}*/}
+            {/*            sx={{*/}
+            {/*                position: "absolute",*/}
+            {/*                top: 7,*/}
+            {/*                right: 335,*/}
+            {/*            }}*/}
+            {/*            onClick={resetForm}*/}
+            {/*    >*/}
+            {/*        Cofnij zmiany*/}
+            {/*    </Button>*/}
+            {/*</Fade>*/}
+        </Box>
+
     );
 }
