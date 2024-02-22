21329d3a03de5d72c685749b7078ad1f36c351c9
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-02-22 11:47:12 +0100

    Aktualizacja

--- resources/js/Theme/Theme.jsx
+++ resources/js/Theme/Theme.jsx
@@ -83,7 +83,7 @@ export default function Theme({children}) {
                 shape: {
                     borderRadius: 16
                 }
-            }),
+            }, plPL),
 
         [prefersDarkMode]
     );
