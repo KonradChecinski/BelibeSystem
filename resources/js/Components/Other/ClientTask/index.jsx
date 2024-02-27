1e166c4fc479fcb0c7905c58e4dad892523cd1f9
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-02-27 17:28:53 +0100

    Zmiana sposobu wyświetlania zadań

---
+++ resources/js/Components/Other/ClientTask/index.jsx
@@ -0,0 +1,177 @@
+import {useEffect, useState} from "react";
+import {
+    Box,
+    Card, CardActions,
+    CardContent, Chip,
+    Divider,
+    Fab,
+    IconButton,
+    Tooltip,
+    Typography,
+} from "@mui/material";
+import {Add, Delete, Edit} from "@mui/icons-material";
+import {useTheme} from "@mui/material/styles";
+import moment from "moment";
+import DeleteClientNotesDialog from "@/Components/Dialogs/ClientDialog/ClientDeleteDialogs/DeleteClientNotesDialog";
+import ClientAddEditNotesDialog from "@/Components/Dialogs/ClientDialog/ClientAddEditDialogs/ClientAddEditNotesDialog";
+import DeleteClientTaskDialog from "@/Components/Dialogs/ClientDialog/ClientDeleteDialogs/DeleteClientTaskDialog";
+import ClientAddEditTasksDialog from "@/Components/Dialogs/ClientDialog/ClientAddEditDialogs/ClientAddEditTasksDialog";
+
+export default function ClientTasks({tasks, readOnly, color, props}) {
+    const theme = useTheme();
+    const [openDialogAdd, setOpenDialogAdd] = useState(false);
+    const [openDialogDelete, setOpenDialogDelete] = useState(tasks.map((task) => ({id: task.id, value: false})));
+    const [openDialogEdit, setOpenDialogEdit] = useState(tasks.map((task) => ({id: task.id, value: false})));
+
+    useEffect(() => {
+        setOpenDialogDelete(tasks.map((task) => ({id: task.id, value: false})))
+        setOpenDialogEdit(tasks.map((task) => ({id: task.id, value: false})))
+    }, [props])
+
+    return (
+        <>
+            <Box sx={{display: 'flex', flexDirection: 'column', gap: 1, mb: 3}}>
+                {tasks.map((task) => {
+
+                    const onEditClick = (e) => {
+                        // setOpenDialogAdd(true)
+                        let object = openDialogEdit.find(e => e.id === task.id)
+                        object.value = !object.value
+
+                        setOpenDialogEdit([...openDialogEdit, object])
+                    };
+
+                    const onDeleteClick = (e) => {
+                        // setOpenDialogDelete(true);
+
+                        let object = openDialogDelete.find(e => e.id === task.id)
+                        object.value = !object.value
+
+                        setOpenDialogDelete([...openDialogDelete, object])
+                    };
+                    return (
+                        <Box key={task.id}>
+                            <Card variant="outlined"
+
+                                  sx={{
+                                      position: "relative",
+                                      px: 1
+                                  }}>
+                                <CardContent>
+                                    <Box sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'space-between',
+                                        gap: 1,
+                                        mb: 1
+                                    }}>
+                                        <Box sx={{
+                                            display: 'inline-flex',
+                                            alignItems: 'center',
+                                            gap: 1,
+                                        }}>
+                                            <Typography
+                                                sx={{
+                                                    fontSize: 18,
+                                                }}>
+                                                {task.title}
+                                            </Typography>
+                                            {/*<Divider orientation="vertical" flexItem/>*/}
+                                        </Box>
+
+                                        <Chip label={moment(task.datetime).format("DD-MM-YYYY HH:mm")}
+                                              variant="outlined"/>
+
+                                    </Box>
+
+                                    <Divider variant="middle"/>
+
+                                    <Typography sx={{
+                                        my: 2
+                                    }}>
+                                        {task.text}
+                                    </Typography>
+                                    <Divider variant="middle"/>
+
+
+                                    <Box sx={{
+                                        display: 'inline-flex',
+                                        gap: 1,
+                                        mt: 1
+                                    }}>
+                                        <Typography>
+                                            {task.user.name}
+                                        </Typography>
+                                        <Divider orientation="vertical" flexItem/>
+                                        <Typography>
+                                            {moment(task.created_at).format("DD-MM-YYYY HH:mm")}
+                                        </Typography>
+                                    </Box>
+
+                                </CardContent>
+                                <CardActions disableSpacing
+                                             sx={{
+                                                 position: "absolute",
+                                                 bottom: 0,
+                                                 right: 10,
+                                             }}
+                                >
+                                    <Tooltip title="Edycja">
+                                        <IconButton aria-label="edit"
+                                                    onClick={onEditClick}
+                                        >
+                                            <Edit/>
+                                        </IconButton>
+                                    </Tooltip>
+
+                                    <Tooltip title="Usuń">
+                                        <IconButton aria-label="delete"
+                                                    onClick={onDeleteClick}
+                                        >
+                                            <Delete/>
+                                        </IconButton>
+                                    </Tooltip>
+                                </CardActions>
+                            </Card>
+
+                            <DeleteClientTaskDialog open={openDialogDelete.find(e => e.id === task.id)?.value}
+                                                    setOpen={() => {
+                                                        let object = openDialogDelete.find(e => e.id === task.id)
+                                                        object.value = !object.value
+
+                                                        setOpenDialogDelete([...openDialogDelete, object])
+                                                    }
+                                                    }
+                                                    task={task} params={props}/>
+
+                            <ClientAddEditTasksDialog open={openDialogEdit.find(e => e.id === task.id)?.value}
+                                                      setOpen={() => {
+                                                          let object = openDialogEdit.find(e => e.id === task.id)
+                                                          object.value = !object.value
+
+                                                          setOpenDialogEdit([...openDialogEdit, object])
+                                                      }}
+                                                      clickedTask={task} params={props}/>
+                        </Box>
+                    )
+                })}
+            </Box>
+
+            {!readOnly ?
+                <>
+                    <Box sx={{position: "absolute", bottom: -10, right: 0, zIndex: 20}}>
+                        <Fab color="primary" aria-label="add" onClick={() => setOpenDialogAdd(true)}>
+                            <Add/>
+                        </Fab>
+
+                    </Box>
+
+                    <ClientAddEditTasksDialog open={openDialogAdd} setOpen={setOpenDialogAdd}
+                                              clickedTask={null} params={props}/>
+                </>
+                : ""
+            }
+
+        </>
+    );
+}
--- resources/js/Components/Pages/Client/ClientTasksComponent/index.jsx
+++ resources/js/Components/Pages/Client/ClientTasksComponent/index.jsx
@@ -13,22 +13,28 @@ import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 import ClientActivityTable from "@/Components/Table/ClientActivityTable";
 import ClientTasksTable from "@/Components/Table/ClientTasksTable";
 import TaskIcon from '@mui/icons-material/Task';
+import ClientNotes from "@/Components/Other/ClientNotes";
+import ClientTasks from "@/Components/Other/ClientTask";
 
 
 export default function ClientTasksComponent(props) {
-    console.log("Propsy: ", props)
+    // console.log("Propsy: ", props)
     return (
         <Box sx={{display: "flex", flexDirection: "column"}}>
 
-            <Typography
-                sx={{mb: 3, display: "flex", gap: 1, alignItems: "center"}}>
-                <TaskIcon fontSize={"large"}/>
-                Zadania klienta
-            </Typography>
+            {/*<Typography*/}
+            {/*    sx={{mb: 3, display: "flex", gap: 1, alignItems: "center"}}>*/}
+            {/*    <TaskIcon fontSize={"large"}/>*/}
+            {/*    Zadania klienta*/}
+            {/*</Typography>*/}
 
             <Box sx={{pr: 0}}>
-                <ClientTasksTable tasks={props.client.tasks} readOnly={!props.editing} props={props}/>
+                <ClientTasks tasks={props.client.tasks} readOnly={!props.editing} props={props}/>
             </Box>
+
+            {/*<Box sx={{pr: 0}}>*/}
+            {/*    <ClientTasksTable tasks={props.client.tasks} readOnly={!props.editing} props={props}/>*/}
+            {/*</Box>*/}
         </Box>
 
     );
