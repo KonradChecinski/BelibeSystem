a634c6a18a2fcaafc757938a090883adf49eaebc
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-03-18 10:38:00 +0100

    Poprawa dodawania zamówień

--- app/Jobs/Shoper/ShoperOrderCreateInSubiekt.php
+++ app/Jobs/Shoper/ShoperOrderCreateInSubiekt.php
@@ -46,9 +46,9 @@ class ShoperOrderCreateInSubiekt implements ShouldQueue
 
         foreach ($orders as $order) {
             $orderProducts = $order->shoperOrderProducts;
-//            dd(mb_substr("SHP " . $order['order_id'] . " - " . iconv("UTF-8", "Windows-1250//IGNORE", $order['firstname']) . " " . iconv("UTF-8", "Windows-1250//IGNORE", $order['lastname']), 0, 30));
+//            dd(mb_substr("SHP " . $order['order_id'] . " - " . iconv("UTF-8", "Windows-1250//IGNORE", $order['firstname']) . " " . iconv("UTF-8", "Windows-1250//IGNORE", $order['lastname']), 0, 29));
             $zamowienie = $subiekt->SuDokumentyManager->DodajZK();
-            $zamowienie->NumerOryginalny = mb_substr("SHP " . $order['order_id'] . " - " . iconv("UTF-8", "Windows-1250//IGNORE", $order['firstname']) . " " . iconv("UTF-8", "Windows-1250//IGNORE", $order['lastname']), 0, 30);
+            $zamowienie->NumerOryginalny = mb_substr("SHP " . $order['order_id'] . " - " . iconv("UTF-8", "Windows-1250//IGNORE", $order['firstname']) . " " . iconv("UTF-8", "Windows-1250//IGNORE", $order['lastname']), 0, 29);
             $zamowienie->LiczonyOdCenBrutto = true;
             $zamowienie->PoziomCenyId = 3;
             $zamowienie->Pozycje->PrzeliczWedlugPoziomuCen();
