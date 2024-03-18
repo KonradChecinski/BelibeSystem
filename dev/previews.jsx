06027f0deac05eab900b7123a8bc39c100533b8c
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-03-18 10:53:20 +0100

    Poprawa dev

--- dev/previews.jsx
+++ dev/previews.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
-import { PaletteTree } from "./palette";
-import Dashboard2 from "@/Pages/Settings/UsersAndPermissions/User";
+import {PaletteTree} from "./palette";
+import Dashboard from "@/Pages/System/Dashboard";
 
 const ComponentPreviews = () => {
     return (
-        <Previews palette={<PaletteTree />}>
-            <ComponentPreview path="/Dashboard2">
-                <Dashboard2/>
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Dashboard">
+                <Dashboard/>
             </ComponentPreview>
         </Previews>
     );
