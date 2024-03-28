df6a832d40b7f13f2e5bf89e6b4a6010e4c8273e
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-03-28 01:01:17 +0100

    Dodanie edycji stron #2

---
+++ resources/js/Pages/System/Pages/functions/index.jsx
@@ -0,0 +1,36 @@
+export const getClassNameFactory =
+    (rootClass, styles, {baseClass = ""} = {}) =>
+        (options = {}) => {
+            let descendant = false;
+            let modifiers = false;
+
+            if (typeof options === "string") {
+                descendant = options;
+            } else if (typeof options === "object") {
+                modifiers = options;
+            }
+
+            if (descendant) {
+                return baseClass + styles[`${rootClass}-${descendant}`] || "";
+            } else if (modifiers) {
+                const prefixedModifiers = {};
+
+                for (let modifier in modifiers) {
+                    prefixedModifiers[styles[`${rootClass}--${modifier}`]] =
+                        modifiers[modifier];
+                }
+
+                const c = styles[rootClass];
+
+                return (
+                    baseClass
+                    // +
+                    // classnames({
+                    //     [c]: !!c, // only apply the class if it exists
+                    //     ...prefixedModifiers,
+                    // })
+                );
+            } else {
+                return baseClass + styles[rootClass] || "";
+            }
+        };
