ebe91dd7417993d84e4ffbf9fc5cdcb59b1b95ae
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-04-18 22:36:26 +0200

    Dodanie strony głównej

---
+++ resources/js/Pages/B2B/MainPage.jsx
@@ -0,0 +1,25 @@
+import {Head, Link} from "@inertiajs/react";
+import ClientLayout from "@/Layouts/ClientLayout";
+import {useSnackbar} from "notistack";
+import {useLaravelReactI18n} from "laravel-react-i18n";
+
+export default function B2bMainPage(props) {
+    const {enqueueSnackbar, closeSnackbar} = useSnackbar();
+    const {t} = useLaravelReactI18n();
+    console.log(props)
+    return (
+        <ClientLayout
+            auth={props.auth}
+            errors={props.errors}
+            categories={props.menu}
+            bgImage={props.backgroundImage}
+            header={
+                t("Dashboard")
+            }
+        >
+            <Head title={t("Dashboard")}/>
+
+
+        </ClientLayout>
+    );
+}
--- routes/b2b/b2b.php
+++ routes/b2b/b2b.php
@@ -1,22 +1,25 @@
 <?php
 
+use App\Http\Controllers\B2B\B2bCartController;
 use App\Http\Controllers\B2B\B2bFavoritesController;
+use App\Http\Controllers\B2B\B2bMainPageController;
 use App\Http\Controllers\B2B\B2bProductCategoryController;
 use App\Http\Controllers\B2B\B2bProductController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get("/", function () {
-    return Inertia::render("B2B/Dashboard");
-})->middleware(["auth:client", "verified"])->name("b2b.dashboard");
 
 Route::middleware(["auth:client", "verified"])->group(function () {
     Route::group([], function () {
+        Route::get("/", [B2bMainPageController::class, 'index'])->name("b2b.dashboard");
+
         Route::get("/c/{slug}", [B2bProductCategoryController::class, 'show'])->name("b2b.category");
         Route::get("/m/{slug}", [B2bProductController::class, 'show'])->name("b2b.model");
+
         Route::get("/model/search", [B2bProductController::class, 'search'])->name("b2b.model.search");
+
         Route::get("/favorites", [B2bFavoritesController::class, 'index'])->name("b2b.favorites");
         Route::patch("/favorite/update/{productModel}", [B2bFavoritesController::class, 'update'])->name("b2b.favorite.update");
+
+        Route::get("/cart", [B2bCartController::class, 'index'])->name("b2b.cart");
 
     });
 
