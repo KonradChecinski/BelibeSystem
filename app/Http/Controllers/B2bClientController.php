2f088227681c4cb38ab474f3051bc0045d9d4e00
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-07-01 16:15:24 +0200

    Strona strefy klienta

--- app/Http/Controllers/B2bClientController.php
+++ app/Http/Controllers/B2bClientController.php
@@ -14,6 +14,7 @@ class B2bClientController extends Controller
     public function index()
     {
         $client = Helper::getClientToB2b();
+        $client->load(["country", "accountManager"]);
 
         return Inertia::render('B2B/ClientZone', [
             'client' => $client
--- resources/js/Pages/B2B/ClientZone.jsx
+++ resources/js/Pages/B2B/ClientZone.jsx
@@ -2,6 +2,19 @@ import {Head, Link} from "@inertiajs/react";
 import ClientLayout from "@/Layouts/ClientLayout";
 import {useSnackbar} from "notistack";
 import {useLaravelReactI18n} from "laravel-react-i18n";
+import {
+    Avatar,
+    Box,
+    Divider,
+    Grid,
+    ListItemIcon,
+    ListItemText,
+    MenuItem,
+    MenuList,
+    Paper,
+    Typography
+} from "@mui/material";
+import {Email, History, Payment, Phone, PhoneAndroid, ReceiptLong} from "@mui/icons-material";
 
 export default function B2bMainPage(props) {
     const {enqueueSnackbar, closeSnackbar} = useSnackbar();
@@ -22,6 +35,137 @@ export default function B2bMainPage(props) {
             }
         >
             <Head title={t("Client zone")}/>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={8}>
+                    <Paper sx={{height: 300, p: 2}}>
+                        <Box>
+                            <Typography variant="h3" component="h4">
+                                {props.client.name}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom component="h4">
+                                NIP: {props.client.nip}
+                            </Typography>
+
+                            <Divider sx={{my: 2}}/>
+
+                            <Typography variant="body1" gutterBottom component="h4">
+                                {props.client.street} {props.client.building_number}{props.client.apartment_number ? "/" + props.client.apartment_number : ""}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom component="h4">
+                                {props.client.postal_code}, {props.client.city}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom component="h4">
+                                {props.client.country.name}
+                            </Typography>
+
+                            <Divider sx={{my: 2}}/>
+                            <Box sx={{display: "flex", flexDirection: "column", gap: 1}}>
+                                <Box sx={{display: "flex", gap: 2, alignItems: "center"}}>
+                                    <Email fontSize="large"/>
+                                    <Typography variant="body1" component="h4">
+                                        {props.client.email}
+                                    </Typography>
+                                </Box>
+                                <Box sx={{display: "flex", gap: 2, alignItems: "center"}}>
+                                    <Phone fontSize="large"/>
+                                    <Typography variant="body1" component="h4">
+                                        {props.client.phone}
+                                    </Typography>
+                                </Box>
+                            </Box>
+
+
+                        </Box>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Paper sx={{height: 300, p: 2}}>
+
+                        <Typography variant="h5" gutterBottom sx={{pl: 1}}>
+                            Skróty
+                        </Typography>
+                        <MenuList sx={{height: 1, display: "flex", flexDirection: "column"}}>
+                            <Link href={route("b2b.orders")}>
+                                <MenuItem sx={{height: 1, m: 1}}>
+                                    <ListItemIcon>
+                                        <History fontSize="large"/>
+                                    </ListItemIcon>
+                                    <ListItemText>
+                                        {t("Orders")}
+                                    </ListItemText>
+                                </MenuItem>
+                            </Link>
+                            <Link href={route("b2b.invoices")}>
+                                <MenuItem sx={{height: 1, m: 1}}>
+                                    <ListItemIcon>
+                                        <ReceiptLong fontSize="large"/>
+                                    </ListItemIcon>
+                                    <ListItemText>
+                                        {t("Invoices")}
+                                    </ListItemText>
+                                </MenuItem>
+                            </Link>
+                            <Link href={route("b2b.settlements")}>
+                                <MenuItem sx={{height: 1, m: 1}}>
+                                    <ListItemIcon>
+                                        <Payment fontSize="large"/>
+                                    </ListItemIcon>
+                                    <ListItemText>
+                                        {t("Settlements")}
+                                    </ListItemText>
+                                </MenuItem>
+                            </Link>
+                        </MenuList>
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={12} md={12}>
+                    <Paper sx={{height: 300, p: 2}}>
+
+                        <Typography variant="h5" gutterBottom component="h5">
+                            Twój opiekun
+                        </Typography>
+                        <Box
+                            sx={{
+                                width: 1,
+                                height: 1,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                gap: 2
+                            }}>
+                            <Box>
+                                <Avatar
+                                    alt={"Zdjęcie opiekuna"}
+                                    src={props.client.account_manager.icon}
+                                    sx={{height: 200, width: 200}}
+                                />
+                            </Box>
+                            <Box>
+
+                                <Typography variant="h2" gutterBottom component="h2">
+                                    {props.client.account_manager.name}
+                                </Typography>
+                                <Box sx={{display: "flex", flexDirection: "column", gap: 1}}>
+                                    <Box sx={{display: "flex", gap: 2, alignItems: "center"}}>
+                                        <Email fontSize="large"/>
+                                        <Typography variant="body1" component="h4">
+                                            {props.client.account_manager.email}
+                                        </Typography>
+                                    </Box>
+                                    <Box sx={{display: "flex", gap: 2, alignItems: "center"}}>
+                                        <PhoneAndroid fontSize="large"/>
+                                        <Typography variant="body1" component="h4">
+                                            {props.client.account_manager.phone}
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
+
+                    </Paper>
+                </Grid>
+            </Grid>
 
 
         </ClientLayout>
