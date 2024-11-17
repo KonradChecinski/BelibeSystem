9755c4a8356f43e65f7d7f9bf6b3f5bf96963f21
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-11-17 22:23:03 +0100

    Poprawa wyświetlania zdjęć na stronie kategorii produktów

--- app/Http/Controllers/B2B/B2bProductCategoryController.php
+++ app/Http/Controllers/B2B/B2bProductCategoryController.php
@@ -66,7 +66,7 @@ class B2bProductCategoryController extends Controller
                     'name' => $model->name,
                     'symbol' => $model->symbol,
                     'slug' => $model->slug,
-                    'mainImages' => $mainImages ? $mainImages->sortBy("main")->map(fn($image) => ["path" => $image->path])->values() : null,
+                    'mainImages' => $mainImages ? $mainImages->sortBy("main")->map(fn($image) => ["slug" => $image->slug])->values() : null,
                     'price' => PriceForClient::getPrice($model, $model->categories, $model->group, $model->brand, $model->prices, $discounts),
 //                    'quantity' => $model->productsToB2bWithoutRelation->sum("quantity"),
                     'icons' => $model->colorIcons,
