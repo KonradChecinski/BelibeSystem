040c0349e6fc2f7bf9f38f9b8aacbc97c4caef6a
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-11-17 23:35:30 +0100

    Dodanie Laravel reverb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,14 +23,11 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    // wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsHost: window.location.hostname,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    // enabledTransports: ['ws', 'wss'],
-    enabledTransports: ['ws'],
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
