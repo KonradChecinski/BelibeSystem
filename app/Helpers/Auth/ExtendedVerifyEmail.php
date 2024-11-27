f98f18e64be575173cc9b644b74246d572728587
KonradChecinski <konradchecinski2012@gmail.com>
2024-11-27 13:18:05 +0100

    Poprawa maila potwierdzającego założenia konta użytkownika

--- app/Helpers/Auth/ExtendedVerifyEmail.php
+++ app/Helpers/Auth/ExtendedVerifyEmail.php
@@ -4,7 +4,10 @@ namespace App\Helpers\Auth;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\URL;
 
 class ExtendedVerifyEmail extends VerifyEmail
 {
@@ -22,5 +25,30 @@ class ExtendedVerifyEmail extends VerifyEmail
             ->line(Lang::get('Please click the button below to verify your email address.'))
             ->action(Lang::get('Verify Email Address'), $url)
             ->line(Lang::get('If you did not create an account, no further action is required.'));
+    }
+
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param mixed $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        if (static::$createUrlCallback) {
+            return call_user_func(static::$createUrlCallback, $notifiable);
+        }
+
+        $url = URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+
+        return str_replace("https://system.belibe.pl", 'https://b2b.belibe.pl', $url);
     }
 }
