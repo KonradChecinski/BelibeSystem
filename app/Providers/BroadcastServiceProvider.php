8a69611e7bc43d82b72201d70fed3324ca53960a
KonradChecinski <konradchecinski2012@gmail.com>
2024-11-29 09:42:18 +0100

    Poprawa połaczenia reverb dla B2B

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -12,7 +12,8 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Broadcast::routes();
+//        Broadcast::routes();
+        Broadcast::routes(['middleware' => ['web', 'auth:user,client']]);
 
         require base_path('routes/channels.php');
     }
