048968de5dfe6a7327fae37c3d0975d2aa876d46
Konrad Chęciński <Konradchecinski2012@gmail.com>
2024-12-10 00:14:27 +0100

    Dodanie komuniaktu w komendzie

--- app/Console/Commands/CreatePWInInwentaryzacjaWarehouse.php
+++ app/Console/Commands/CreatePWInInwentaryzacjaWarehouse.php
@@ -36,7 +36,7 @@ class CreatePWInInwentaryzacjaWarehouse extends Command
      */
     public function handle()
     {
-        $magIds = [33, 9, 30, 43, 1, 29, 5, 3, 32, 28];
+        $magIds = $this->ask('WarehouseId?', [33, 9, 30, 43, 1, 29, 5, 3, 32, 28]);
 
         $subiekt = app(Subiekt::class)->getInstance();
         $subiekt = $subiekt->connect();
@@ -58,6 +58,7 @@ class CreatePWInInwentaryzacjaWarehouse extends Command
                 $pozycja->Jm = (string)$towar->tw_JednMiary;
                 $pozycja->CenaNettoPrzedRabatem = (float)$towar->cena;
                 $pozycja->IloscJm = (int)$towar->ilosc;
+                $this->info("Dodano towar: " . $towar->tw_symbol . " w ilości: " . $towar->ilosc);
             }
 
 
