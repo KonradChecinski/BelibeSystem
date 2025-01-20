2881dbe1dd741509bca3b8289fa2925ea65eeed6
Konrad Chęciński <Konradchecinski2012@gmail.com>
2025-01-20 23:15:03 +0100

    Zmiana wyglądu strony rejestracji oraz przypomnij haslo

--- app/Http/Controllers/Auth/b2b/RegisteredUserController.php
+++ app/Http/Controllers/Auth/b2b/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth\b2b;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -15,6 +16,11 @@ class RegisteredUserController extends Controller
     {
         return Inertia::render('B2B/Register', [
             'routeLogin' => 'b2b.login',
+            'accountManagers' => User::query()
+                ->where("account_manager", true)
+                ->where("active", true)
+                ->orderBy("lastname")
+                ->get(["firstname", "lastname", "email", "phone"]),
 //            'routeRegister' => 'b2b.register',
         ]);
     }
--- resources/js/Theme/Theme.jsx
+++ resources/js/Theme/Theme.jsx
@@ -10,89 +10,6 @@ import {useMemo} from "react";
 export default function Theme({children}) {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
-    let lightTheme = {
-        primary: {
-            // main: "#1A2035",
-            main: "#1D71B8",
-            second: "rgba(0,0,0,0.08)",
-            third: "rgba(0,0,0,0.25)"
-        },
-        secondary: {main: "#2a53d7"},
-        background: {
-            background: "#f3f4f6",
-            default: "#f3f4f6",
-            paper: "#ffffff",
-            card: "#ffffff",
-            toolbar: "#ffffff"
-        },
-        errorBg: {
-            main: "#ff00001a",
-        },
-        successBg: {
-            main: "#00ad1b66",
-        },
-        gradient: {
-            divider:
-                "linear-gradient(90deg, #038ce3 0%, #014875 50%, #038ce3 100%)"
-        },
-        menuText: {
-            main: "#ffffff",
-        },
-        barcodes: {
-            background: "rgba(243, 244, 246, 0.38)",
-        },
-        field: {
-            border: "rgb(229, 231, 235)"
-        },
-        disabled: {
-            background: "rgba(181,181,181,0.33)"
-        },
-        hoveredCell: {
-            background: "rgba(4,119,243,0.26)"
-        }
-    };
-
-    let darkTheme = {
-        primary: {
-            // main: "#1A2035",
-            main: "#e4e6ee",
-            second: "rgba(255,255,255,0.08)",
-            third: "rgba(255,255,255,0.25)"
-        },
-        secondary: {main: "#0072ba"},
-        background: {
-            background: "#111827",
-            default: "#111827",
-            paper: "#1f2937",
-            card: "#1f2937",
-            toolbar: "#1f2937"
-        },
-        errorBg: {
-            main: "#ff00001a",
-        },
-        successBg: {
-            main: "#00ad1b66",
-        },
-        gradient: {
-            divider:
-                "linear-gradient(90deg, #1f2937 0%, #2a3441 50%, #1f2937 100%)"
-        },
-        menuText: {
-            main: "#ffffff",
-        },
-        barcodes: {
-            background: "rgba(17, 24, 39, 0.38)",
-        },
-        field: {
-            border: "rgba(229, 231, 235, 0.3)"
-        },
-        disabled: {
-            background: "rgba(124,124,124,0.33)"
-        },
-        hoveredCell: {
-            background: "rgba(4,119,243,0.26)"
-        }
-    };
 
     const theme = useMemo(
         () =>
@@ -153,3 +70,88 @@ export default function Theme({children}) {
         </ThemeProvider>
     );
 }
+
+
+export const lightTheme = {
+    primary: {
+        // main: "#1A2035",
+        main: "#1D71B8",
+        second: "rgba(0,0,0,0.08)",
+        third: "rgba(0,0,0,0.25)"
+    },
+    secondary: {main: "#2a53d7"},
+    background: {
+        background: "#f3f4f6",
+        default: "#f3f4f6",
+        paper: "#ffffff",
+        card: "#ffffff",
+        toolbar: "#ffffff"
+    },
+    errorBg: {
+        main: "#ff00001a",
+    },
+    successBg: {
+        main: "#00ad1b66",
+    },
+    gradient: {
+        divider:
+            "linear-gradient(90deg, #038ce3 0%, #014875 50%, #038ce3 100%)"
+    },
+    menuText: {
+        main: "#ffffff",
+    },
+    barcodes: {
+        background: "rgba(243, 244, 246, 0.38)",
+    },
+    field: {
+        border: "rgb(229, 231, 235)"
+    },
+    disabled: {
+        background: "rgba(181,181,181,0.33)"
+    },
+    hoveredCell: {
+        background: "rgba(4,119,243,0.26)"
+    }
+};
+
+export const darkTheme = {
+    primary: {
+        // main: "#1A2035",
+        main: "#e4e6ee",
+        second: "rgba(255,255,255,0.08)",
+        third: "rgba(255,255,255,0.25)"
+    },
+    secondary: {main: "#0072ba"},
+    background: {
+        background: "#111827",
+        default: "#111827",
+        paper: "#1f2937",
+        card: "#1f2937",
+        toolbar: "#1f2937"
+    },
+    errorBg: {
+        main: "#ff00001a",
+    },
+    successBg: {
+        main: "#00ad1b66",
+    },
+    gradient: {
+        divider:
+            "linear-gradient(90deg, #1f2937 0%, #2a3441 50%, #1f2937 100%)"
+    },
+    menuText: {
+        main: "#ffffff",
+    },
+    barcodes: {
+        background: "rgba(17, 24, 39, 0.38)",
+    },
+    field: {
+        border: "rgba(229, 231, 235, 0.3)"
+    },
+    disabled: {
+        background: "rgba(124,124,124,0.33)"
+    },
+    hoveredCell: {
+        background: "rgba(4,119,243,0.26)"
+    }
+};
