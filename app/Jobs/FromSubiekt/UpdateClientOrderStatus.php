9cb1f8a47e89eae6505acac16dec0b933fe63089
KonradChecinski <konradchecinski2012@gmail.com>
2025-07-15 13:47:12 +0200

    Poprawa warunków aktualizacji stanu zamówienia

--- app/Jobs/FromSubiekt/UpdateClientOrderStatus.php
+++ app/Jobs/FromSubiekt/UpdateClientOrderStatus.php
@@ -38,55 +38,95 @@ class UpdateClientOrderStatus implements ShouldQueue, ShouldBeUnique
     public function handle(): void
     {
         $orders = ClientOrder::query()->where("status", 90)->get();
+
         foreach ($orders as $order) {
-            $subiektFV = DB::connection("subiekt")
-                ->table("dok__Dokument")
-                ->where("dok_DoDokId", $order->subiekt_id)
-                ->whereIn("dok_Typ", [2, 21])
-                ->where("dok_Podtyp", 0)
-                ->first([
-                    "dok_Id",
-                    "dok_Typ",
-                    "dok_NrPelny",
-                    "dok_Status",
-                    "dok_StatusFiskal"
-                ]);
+            $subiektOrder = $this->getSubiektOrder($order->subiekt_id);
 
-
-//            dd($order, $subiektFV);
-            if ($subiektFV == null) {
-                if (Carbon::now()->diffInDays($order->created_at) > 7) {
-                    $order->update([
-                        "status" => 0
-                    ]);
-                } else {
-                    continue;
-                }
-            }
-            $subiektFVPrinted = DB::connection("subiekt")
-                ->table("dok_StatusWydruku")
-                ->where("dsw_IdDokumentu", $subiektFV->dok_Id)
-                ->first();
-
-
-//            dd($subiektFV,$subiektFVPrinted, $subiektFV->dok_Status,
-//                (int)$subiektFV->dok_Status === 1, !is_null($subiektFVPrinted));
-
-            if (
-                (int)$subiektFV->dok_Status == 1 //skutek 0-cofnięty, 1-wywołany, 3-odłożony
-                &&
-                (
-                    ((int)$subiektFV->dok_Typ === 2 && !is_null($subiektFVPrinted))
-                    ||
-                    ((int)$subiektFV->dok_Typ === 21 && (int)$subiektFV->dok_StatusFiskal === 1)
-                )
-            ) {
-                $order->update([
-                    "status" => 100
-                ]);
-                $order->client->accountManager->notify(new OrderCompleatedUser($order));
+            if (!$subiektOrder) {
+                $this->handleMissingSubiektOrder($order);
+                continue;
             }
 
+            if ($this->isOrderCompletedWithoutFV($subiektOrder)) {
+                $this->completeOrder($order);
+                continue;
+            }
+
+            $subiektFV = $this->getSubiektFV($order->subiekt_id);
+
+            if (!$subiektFV) {
+                $this->handleMissingSubiektOrder($order);
+                continue;
+            }
+
+            if ($this->isOrderCompletedWithFVCompleted($subiektFV)) {
+                $this->completeOrder($order);
+            }
         }
+    }
+
+    private function getSubiektOrder(int $subiektId)
+    {
+        return DB::connection("subiekt")
+            ->table("dok__Dokument")
+            ->where("dok_Id", $subiektId)
+            ->whereIn("dok_Typ", [16])
+            ->first([
+                "dok_Id",
+                "dok_Typ",
+                "dok_Podtyp",
+                "dok_DoDokId",
+                "dok_NrPelny",
+                "dok_Status",
+                "dok_StatusFiskal"
+            ]);
+    }
+
+    private function handleMissingSubiektOrder(ClientOrder $order): void
+    {
+        if (Carbon::now()->diffInDays($order->created_at) > 7) {
+            $order->update(["status" => 0]);
+        }
+    }
+
+    private function isOrderCompletedWithoutFV($subiektOrder): bool
+    {
+        return $subiektOrder->dok_Status == 8 && is_null($subiektOrder->dok_DoDokId);
+    }
+
+    private function completeOrder(ClientOrder $order): void
+    {
+        $order->update(["status" => 100]);
+        $order->client->accountManager->notify(new OrderCompleatedUser($order));
+    }
+
+    private function getSubiektFV(int $subiektId)
+    {
+        return DB::connection("subiekt")
+            ->table("dok__Dokument")
+            ->where("dok_DoDokId", $subiektId)
+            ->whereIn("dok_Typ", [2, 21])
+            ->where("dok_Podtyp", 0)
+            ->first([
+                "dok_Id",
+                "dok_Typ",
+                "dok_NrPelny",
+                "dok_Status",
+                "dok_StatusFiskal"
+            ]);
+    }
+
+    private function isOrderCompletedWithFVCompleted($subiektFV): bool
+    {
+        $subiektFVPrinted = DB::connection("subiekt")
+            ->table("dok_StatusWydruku")
+            ->where("dsw_IdDokumentu", $subiektFV->dok_Id)
+            ->first();
+
+        return (int)$subiektFV->dok_Status === 1 &&
+            (
+                ((int)$subiektFV->dok_Typ === 2 && !is_null($subiektFVPrinted)) ||
+                ((int)$subiektFV->dok_Typ === 21 && (int)$subiektFV->dok_StatusFiskal === 1)
+            );
     }
 }
